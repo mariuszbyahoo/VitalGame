5f96ee2e977e7af275be86b13f2d37399736689d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-03-22 23:40:55 +0100

    Adding the changeStatus(...) test cases in the CellTest class

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -3,6 +3,33 @@ import org.junit.*;
 public class CellTest {
 
     @Test
+    public void changeStatusOneNeighbourTest(){
+        Cell cell = new Cell(Life.ALIVE);
+        cell.changeStatus(1);
+        Assert.assertEquals(Life.DEAD, cell.getStatus());
+    }
+
+    @Test
+    public void changeStatusTwoNeighboursTest(){
+        Cell cell = new Cell(Life.ALIVE);
+        cell.changeStatus(2);
+        Assert.assertEquals(Life.ALIVE, cell.getStatus());
+    }
+
+    @Test
+    public void changeStatusThreeNeighboursTest() {
+        Cell cell = new Cell(Life.ALIVE);
+        cell.changeStatus(3);
+        Assert.assertEquals(Life.ALIVE, cell.getStatus());
+    }
+
+    @Test
+    public void changeStatusFourNeighboursTest(){
+        Cell cell = new Cell(Life.ALIVE);
+        cell.changeStatus(4);
+        Assert.assertEquals(Life.DEAD, cell.getStatus());
+    }
+    @Test
     public void setStatusTest(){
         Cell cell = new Cell(Life.ALIVE);
         cell.setStatus(Life.DEAD);
