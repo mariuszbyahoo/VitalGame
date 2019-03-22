0890d4939b10c811f3aab82c9801a346a3e0f394
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-03-22 21:48:12 +0100

    Adding a cell's coordinates to the Cell class

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,8 +1,11 @@
 /**
- * Class representing a Cell, witch may be alive, or dead
+ * Class representing a Cell, witch may be alive, or dead, an amount of its neighbours and its position
  */
 public class Cell {
     private Life status;
+    private int i;
+    private int j;
+    private int neighboursAmount;
 
     public Cell (Life status){
         setStatus(status);
@@ -22,5 +25,53 @@ public class Cell {
      */
     public Life getStatus() {
         return status;
+    }
+
+    /**
+     * returns an amount of the cell's neighbours
+     * @return
+     */
+    public int getNeighboursAmount() {
+        return neighboursAmount;
+    }
+
+    /**
+     * sets an amount of the cell's neighbours
+     * @param neighboursAmount
+     */
+    public void setNeighboursAmount(int neighboursAmount) {
+        this.neighboursAmount = neighboursAmount;
+    }
+
+    /**
+     * returns the cell's "i" coordinate
+     * @return
+     */
+    public int getI() {
+        return i;
+    }
+
+    /**
+     * sets the cell's "i" coordinate
+     * @param i
+     */
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    /**
+     * returns the "j" coordinate
+     * @return
+     */
+    public int getJ() {
+        return j;
+    }
+
+    /**
+     * sets the "j" coordinate
+     * @param j
+     */
+    public void setJ(int j) {
+        this.j = j;
     }
 }
